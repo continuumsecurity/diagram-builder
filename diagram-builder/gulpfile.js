d731773885f4267f8b6c337148b5f81837518d2b
najorcruzcruz <najorcruzcruz@gmail.com>
2018-11-21 12:44:32 +0000

    [IR-2501] Fixes demo problem. Add gulp clean task.

--- diagram-builder/gulpfile.js
+++ diagram-builder/gulpfile.js
@@ -2,16 +2,22 @@ var gulp = require('gulp');
 var del = require('del');
 var log = require('fancy-log');
 
-var AlloySource = "target/bower_components/alloy-ui/build/**";
-var DiagramDest = "src/main/resources/org/vaadin/diagrambuilder/public/diagram-builder/js/diagram/alloyui/build";
+var alloySource = "target/bower_components/alloy-ui/build/**";
+var diagramFolderDest = "src/main/resources/org/vaadin/diagrambuilder/public/diagram-builder/js/diagram/alloyui/build";
+var nodeModulesFolder = "node_modules";
 
 gulp.task('clean', function () {
-   log.info("CLEANING Diagram Destination folder");
-   return del.sync([DiagramDest]);
+    log.info("CLEANING  node modules");
+    return del([nodeModulesFolder]);
 });
 
-gulp.task('build', ['clean'], function() {
+gulp.task('sync', function () {
+   log.info("SYNC Diagram Destination folder");
+   return del.sync([diagramFolderDest]);
+});
+
+gulp.task('build', ['sync'], function() {
     log.info("COPYING Alloy Source into Diagram Destination");
-    return gulp.src(AlloySource)
-        .pipe(gulp.dest(DiagramDest));
+    return gulp.src(alloySource)
+        .pipe(gulp.dest(diagramFolderDest));
 });
