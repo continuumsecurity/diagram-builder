bc7b87820eae5c535b55f8982bd94fdd08c7208c
najorcruzcruz <najorcruzcruz@gmail.com>
2019-11-18 09:07:06 +0000

    [IR-4092] Update new AlloyUI version to 4.2.0

--- diagram-builder/src/main/resources/org/vaadin/diagrambuilder/public/diagram-builder/js/diagram/alloyui/build/aui-form-builder-field-sentence/aui-form-builder-field-sentence-min.js
+++ diagram-builder/src/main/resources/org/vaadin/diagrambuilder/public/diagram-builder/js/diagram/alloyui/build/aui-form-builder-field-sentence/aui-form-builder-field-sentence-min.js
@@ -1,1 +1,1 @@
-YUI.add("aui-form-builder-field-sentence",function(e,t){e.FormBuilderFieldSentence=e.Base.create("form-builder-field-sentence",e.FormField,[e.FormBuilderFieldBase])},"4.1.2",{requires:["aui-form-builder-field-base","aui-form-field"]});
+YUI.add("aui-form-builder-field-sentence",function(e,t){e.FormBuilderFieldSentence=e.Base.create("form-builder-field-sentence",e.FormField,[e.FormBuilderFieldBase])},"4.2.0",{requires:["aui-form-builder-field-base","aui-form-field"]});
